1368a558fde4a654b4122f3993fd3a3431c4f24d
Flavia Monteiro <103121161+FlaviaBMonteiro@users.noreply.github.com>
2023-08-21 13:34:18 -0300

    Update database.js

--- js/database.js
+++ js/database.js
@@ -3,7 +3,7 @@
 const data = [
   {
     id: 1,
-    img: "../img/Bolsa_01.png",
+    img: "./img/Bolsa_01.png",
     nameItem: "Bolsa Lateral Preta",
     description:
       "Adicione um pouco de energia felina ao seu guarda-roupa com esta bolsa vibrante",
@@ -13,7 +13,7 @@ const data = [
   },
   {
     id: 2,
-    img: "../img/Bolsa_02.png",
+    img: "./img/Bolsa_02.png",
     nameItem: "Bolsa Lateral Listrada",
     description:
       "Adicione o charme das listras com o amor felino e esteja pronta para qualquer ocasião",
@@ -23,7 +23,7 @@ const data = [
   },
   {
     id: 3,
-    img: "../img/Roupa_01.png",
+    img: "./img/Roupa_01.png",
     nameItem: "Moletom Com Capuz",
     description:
       "Nesse inverno nada melhor que o charme felino te acompanhando o tempo todo",
@@ -33,7 +33,7 @@ const data = [
   },
   {
     id: 4,
-    img: "../img/Caneca_02.png",
+    img: "./img/Caneca_02.png",
     nameItem: "Caneca Com Rabinho",
     description:
       "Beba seu café nessa charmosa caneca como alça em formato de rabinho *.*",
@@ -43,7 +43,7 @@ const data = [
   },
   {
     id: 5,
-    img: "../img/Bolsa_03.png",
+    img: "./img/Bolsa_03.png",
     nameItem: "Bolsa Com Alça Preta",
     description:
       "Use essa linda bolsa caprichada nos detalhes para mostrar ao mundo seu amor pelos bichanos",
@@ -53,7 +53,7 @@ const data = [
   },
   {
     id: 6,
-    img: "../img/Roupa_02.png",
+    img: "./img/Roupa_02.png",
     nameItem: "Blusa Estampada",
     description:
       "Essa estampa charmosa somada ao charme felino é tudo que voce precisa para arrasar nesse verão",
